e22453c4bd1b31e80e3144a5cac55fa16ee80ace
kizz <zp.zoki95@gmail.com>
2017-12-24 00:24:37 +0100

    Seq. version of merge sort finished

---
+++ merge.cpp
@@ -0,0 +1,87 @@
+#include <iostream>
+using namespace std;
+
+
+// print array 
+void printArray(int *a, int start, int end)
+{
+    for(int i = start; i < end; i++)
+        cout << a[i] << " ";
+    cout << endl;
+}
+
+//init
+void initArray(int *a, int n)
+{
+    for(int i = 0; i < n; i++)
+        a[i] = (n-1) - i;
+    
+    cout << "Array: ";
+    printArray(a, 0, n);
+}
+
+//merge right and left
+void merge(int *dst, int s,int m, int e)
+{
+    //cout << "Start of merging" << endl;
+    //printArray(dst, s, e);
+    int temp[e-s];
+    int il = s, ir = m, it = 0;
+    while( il < m && ir < e)
+    {
+        if(dst[il] < dst[ir])
+            temp[it++] = dst[il++];
+        else
+            temp[it++] = dst[ir++];
+    }
+    
+    while( il < m)
+    {
+        temp[it++] = dst[il++];
+    }
+    
+    while( ir < e)
+    {
+        temp[it++] = dst[ir++];
+    }
+    
+    //copying result
+    for(int i = 0; i < (e-s); i++)
+        dst[s+i] = temp[i];
+    
+    
+    //cout << "After merging" << endl;
+   // printArray(dst, s, e);
+}
+
+//to devide array 
+void devide(int *a, int s, int e)
+{
+    if( e - s == 1 )
+    {
+       // printArray(a, s, e);
+        return;
+    }
+    else 
+    {
+        int m = (s+e)/2;
+        devide(a, s, m);
+        devide(a, m, e);
+        merge(a, s, m, e);
+
+    }
+}
+
+int main() 
+{
+    int *array;
+    int n;
+    cout << "Input size of array:";
+    cin >> n;
+    
+    array = new int[n];
+    
+    initArray(array, n);
+    
+    devide(array, 0, n);
+}
